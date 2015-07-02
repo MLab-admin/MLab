c785d90d81971d91c3aca21ae3b0e790fb426f9a
Raphaël Candelier <raphael.candelier@upmc.fr>
2015-07-02 11:50:20 +0200

    Update startup and update settings

--- +ML/+Config/default.m
+++ +ML/+Config/default.m
@@ -1,26 +1,25 @@
 function out = default(varargin)
-%ML.Config.default Set MLab configuration structure to default
-%   C = ML.CONFIG.DEFAULT() sets the MLab configuration structure to its
-%   default value and returns it.
+%ML.Config.default Reset default configuration
+%   ML.Config.default() resets MLab configuration to default.
 %
-%   C = ML.CONFIG.GET(..., 'cfile', CFILE) uses the tag CFILE to save in
-%   the PREFDIR directory. The default value is 'MLab'.
+%   ML.Config.default(..., 'cfile', CFILE) saves the default 
+%   configuration in a specific file in the PREFDIR directory. The default 
+%   value of CFILE is 'MLab'.
 %
-%   C = ML.CONFIG.GET(..., 'quiet', false) displays a creation message.
+%   ML.Config.default(..., 'quiet', false) displays a creation message.
+%
+%   C = ML.Config.default(...) returns the configuration structure.
 %
 %   See also ML.config.
 %
-%   Reference page in Help browser: <a href="matlab:doc ML.Config.get">doc ML.Config.get</a>
-%   <a href="matlab:doc ML">MLab documentation</a>
+%   More on <a href="matlab:ML.doc('ML.Config.default');">ML.doc</a>
 
 % === Input variables =====================================================
 
-in = inputParser;
-in.addParamValue('cfile', 'MLab', @ischar);
-in.addParamValue('quiet', true, @islogical);
-
-in.parse(varargin{:});
-in = in.Results;
+in = ML.Input;
+in.cfile('MLab') = @ischar;
+in.quiet(true) = @islogical;
+in = +in;
 
 % -------------------------------------------------------------------------
 
@@ -36,26 +35,23 @@ config.charset = 'UTF8';
 tmp = mfilename('fullpath');
 config.path = tmp(1:end-19);
 
-config.startup = struct(...
-    'check_updates', true, ...
-    'autostart', true);
+config.startup = struct('autostart', true, ...
+    'update', true, ...
+    'disp_message', true, ...
+    'message', 'Hello <user:name>');
 
-config.updates = struct(...
-    'mirror_url', 'http://www.candelier.fr/MLab/', ...
-    'check_delete', true);
+config.updates = struct('repository', 'https://github.com/MLab-admin/MLab.git');
 
 config.user = struct(...
     'name', '', ...
     'email', '');
-
-config.plugins = struct();
 
 % --- Save configuration structure
 save(fname, 'config');
 
 % --- Message display
 if ~in.quiet
-    display('The configuration file has been reset to default.');
+    printf('The configuration file ''%s'' has been reset to default.', in.cfile);
 end
 
 % --- Output
