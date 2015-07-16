a0da4d7bb03dfd1ae3e8ed914e3312bf31f866ca
Raphaël Candelier <raphael.candelier@upmc.fr>
2015-07-16 09:51:15 +0200

    First version of ML.Updates.list (commited for debugging purpose)

--- +ML/+Updates/list.m
+++ +ML/+Updates/list.m
@@ -7,6 +7,8 @@ function out = list(varargin)
 % NB: remember to mlock this file when done.
 % NB: remember to do ML.Updates.list('clear', true) after each plugin install.
 
+clc
+
 % --- Inputs
 
 in = ML.Input;
@@ -37,7 +39,7 @@ config = ML.config;
 if isempty(in.what)
     
     if ~exist('plist', 'var')
-        plist = ML.Plugins.list; 
+        plist = ML.Plugins.list;
     end
     what = [{'MLab'} plist];
     
@@ -50,13 +52,15 @@ else
     what = in.what;
     
 end
-    
+
 % --- Check list
- 
+
 for i = 1:numel(what)
     
     if isempty(list.(what{i})) || in.check
-
+        
+        list.(what{i}) = {};
+        
         switch what{i}
             case 'MLab'
                 gname = java.io.File([config.path '.git']);
@@ -64,19 +68,33 @@ for i = 1:numel(what)
                 gname = java.io.File([config.path 'Plugins' filesep what{i} filesep '.git']);
         end
         
+        % --- Git diff
         Git = org.eclipse.jgit.api.Git.open(gname);
-        res = Git.diff.call
+        cmd = Git.diff;
+        repo = Git.getRepository;
+        reader = repo.newObjectReader;
+        cmd.setOldTree(org.eclipse.jgit.treewalk.CanonicalTreeParser([], reader, repo.resolve('master^{tree}')));
+        cmd.setNewTree(org.eclipse.jgit.treewalk.CanonicalTreeParser([], reader, repo.resolve('origin/master^{tree}')));
+        res = cmd.call.toArray;
+                
+        % --- Process result
+        for j = 1:numel(res)
+            tmp = char(res(j));
+            list.(what{i}){end+1} = tmp(11:end-1);
+        end
         
     end
     
 end
-    
-    
+
+list
+
+
 %     || in.check
-%     
-%     
-%     
-%     
+%
+%
+%
+%
 % end
 
 % --- Output
